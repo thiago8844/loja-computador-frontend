70f78883bfd75f6685e055f27e55af1923da1df2
Thiago Machado <thiagoaxe4@gmail.com>
2023-06-13 15:09:53 -0300

    Setup do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+//Componentes
+import Header from "./components/Header/Index";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//Imagens
+
+
+function Header() {
+  return (
+    <header>
+      <img src="/logo 2 degradê+texto Horizontal.svg" alt="" />
+      <p>aaaaaaaaaa</p>
+    </header>
+  );
+}
+
+export default Header;
